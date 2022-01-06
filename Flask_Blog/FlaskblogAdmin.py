486074d39725871e38581280e20bb667f5f1b7f1
EgoBob <96198749+EgoBob@users.noreply.github.com>
2022-01-06 14:22:20 -0600

    Admin login path, needs admin DB to reference

---
+++ Flask_Blog/FlaskblogAdmin.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+from flask_login import login_required
+
+app = Flask(__name__)
+
+@app.route("/login", methods=['GET', 'POST'])
+@login_required
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('Welcom, adminstrator', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
